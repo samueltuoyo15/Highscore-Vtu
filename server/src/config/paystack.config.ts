49474bde22512d476d041bb6a13b951ae7f3cca1
Samuel Tuoyo <samueltuoyo9082@gmail.com>
2025-10-25 20:02:08 +0100

    Delete server directory

--- server/src/config/paystack.config.ts
+++
@@ -1,7 +0,0 @@
-import Paystack from "paystack"
-import dotenv from "dotenv"
-dotenv.config()
-
-const paystack = Paystack(process.env.PAYSTACK_SECRET_KEY as string)
-
-export default paystack
--- server/src/middlewares/error.handler.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorRequestHandler } from "express"
-import logger from "@/utils/logger"
-
-const errorHandler: ErrorRequestHandler = (err, req, res) => {
-  logger.error(err.stack)
-  res.status(err.status || 500).json({message: err.message || "internal server error"})
-}
-
-export default errorHandler
